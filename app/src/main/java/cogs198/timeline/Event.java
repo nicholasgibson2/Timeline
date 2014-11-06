6e3c7b2615dd420d329871e43e0fd65aad9b74ca
Nick <nicholasgibson2@gmail.com>
2014-11-05 16:59:54 -0800

    commenting

--- app/src/main/java/cogs198/timeline/Event.java
+++ app/src/main/java/cogs198/timeline/Event.java
@@ -9,60 +9,69 @@ import java.util.Date;
 
 
 class Event {
-    Date start;
-    long startEpoch;
-    Date end;
-    long today = 1414369636;
+    Date start; //used to easily access day, hour, minute etc.
+    Date end; //currently unused
     int priority;
-    int position = 0;
-    int size;
-    int gap = 20;
-    int minDistance = 60;
-    int maxDistance = 1000;
-    int hourSize = 70;
-
-    int lowSize = 20;
-    int medSize = 35;
-    int highSize = 50;
-
-    static int width, height;
     String title;
+    long startEpoch; //used for actual time comparisons
+
+    int position = 0;
+    int size; //defined by priority
+    int gapSize = 20;
+    int minDistance = 60; //minimum event spacing
+    int maxDistance = 1000; //maximum event spacing
+    int hourSize = 70; //event spacing per hour
+
+    int lowSize = 20; //low priority event size
+    int medSize = 35; //medium priority event size
+    int highSize = 50; //high priority event size
+
+    static int width, height; //width and height of canvas
 
     static Canvas canvas;
     static Paint paint;
-    static int nodeOffset = 0;
+    static int nodeOffset = 0; //used for relative event spacing
 
-    Event prevNode = this;
-    Event nextNode = this;
+    int rectColor = Color.parseColor("#c9e5e3");
+    int eventColor;
+    int textColor = Color.parseColor("#ffffff");
+    int textSize = 32;
+
+    Event prevNode = this; //previous event, default value for this event as first event
+    Event nextNode = this; //next event, default value for this event as first event
 
     Event(String setTitle, long setStart, long setEnd, int setPriority, Event setPrevNode, Event setNextNode) {
-        today *= 1000;
         priority = setPriority;
         start = new Date(setStart);
-        startEpoch = setStart;
         end = new Date(setEnd);
         title = setTitle;
+        startEpoch = setStart;
 
-        if (setPrevNode != null)
+        if (setPrevNode != null) //if this is not the first event
         {
             prevNode = setPrevNode;
             nextNode = setNextNode;
         }
         switch (priority) {
-
             case 0:
                 size = lowSize;
+                eventColor = Color.parseColor("#E5F3FF");
                 break;
             case 1:
                 size = medSize;
+                eventColor = Color.parseColor("#E5F3FF");
                 break;
             case 2:
                 size = highSize;
+                eventColor = Color.parseColor("#E5F3FF");
                 break;
-            default: size = highSize;
+            default:
+                size = highSize;
+                eventColor = Color.parseColor("#E5F3FF");
                 break;
         }
     }
+    //this method is always called from the head node, it will never be called with no events
     public Event addEvent(String setTitle, long setStart, long setEnd, int setPriority, Event head)
     {
         if (nextNode == this && prevNode == this) //only one Node
@@ -75,7 +84,7 @@ class Event {
             else
                 return nextNode;
         }
-        if (setStart > startEpoch)
+        if (setStart > startEpoch) //new event start > old event start
         {
             if (nextNode != head)
                 return nextNode.addEvent(setTitle, setStart, setEnd, setPriority, head);
@@ -86,7 +95,7 @@ class Event {
                 return head;
             }
         }
-        else //setDate.startDate < startDate
+        else //new event start <= old event start
         {
             Event oldPrev = prevNode;
             prevNode = new Event(setTitle, setStart, setEnd, setPriority, oldPrev, this);
@@ -98,6 +107,7 @@ class Event {
                 return head;
         }
     }
+    //set static variables, called once from head node
     void setCanvas(Canvas setCanvas, Paint setPaint, int setWidth, int setHeight) {
         canvas = setCanvas;
         width = setWidth;
@@ -105,9 +115,8 @@ class Event {
         paint = setPaint;
     }
 
+    //initially called from head, called recursively until events are off the display
     void draw(int offset, Event head) {
-
-
 
         if (this == head) {
             position = 100 + offset;
@@ -126,11 +135,12 @@ class Event {
             }
         }
 
-        int rectTop = position - size - gap;
-        int rectBottom = position + size + gap;
+        int rectTop = position - size - gapSize;
+        int rectBottom = position + size + gapSize;
         int rectLeft = width / 2 - 6 - 90;
         int rectRight = width / 2 + 6 - 90;
 
+        //set up date text
         int startMin = start.getMinutes();
         String startText;
         if (startMin == 0) {
@@ -139,21 +149,28 @@ class Event {
         else {
             startText = Integer.toString(start.getHours()) + ":" + Integer.toString(startMin);
         }
-        //Typeface font = Typeface.create("Helvetica",Typeface.BOLD);
-        // paint.setTypeface(font);
 
-        paint.setColor(Color.parseColor("#c9e5e3"));
+        //draw background color rectangle to make gap in timeline around ends of event
+        paint.setColor(rectColor);
         canvas.drawRect(rectLeft, rectTop, rectRight, rectBottom, paint);
-        paint.setColor(Color.parseColor("#E5F3FF"));
+
+        //draw event
+        paint.setColor(eventColor);
         canvas.drawCircle(width/2 - 90, position, size, paint);
 
-        paint.setColor(Color.WHITE);
-        paint.setTextSize(32);
+        //set text properties
+        paint.setColor(textColor);
+        paint.setTextSize(textSize);
 
+        //draw title text on right hand side
         canvas.drawText(title, width / 2 + 60 - 90, position + 13, paint);
+
+        //draw date text on left hand side
         canvas.drawText(startText, width / 2 - 90 - 120, position + 13, paint);
 
-        if (nextNode != head)
+        if (position > height) //event is off the screen
+            return;
+        else if (nextNode != head) //this is not the last event
             nextNode.draw(offset, head);
 
     }
